58d00738bde67717273257d7266d44ff8e87ea86
Alexandra Cordeiro Campos <srcoelho007@gmail.com>
2025-05-10 09:02:05 -0300

    Atualizado repositório

--- backend/core/models.py
+++ backend/core/models.py
@@ -7,15 +7,28 @@ class Anuncio(models.Model):
         ('servico', 'Serviço')
     ]
 
-    #campo da tabela
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
-    titulo = models.CharField(max_lenght=100)
+    titulo = models.CharField(max_length=100)
     descricao = models.TextField()
     preco = models.DecimalField(max_digits=10, decimal_places=2)
-    tipo = models.CharField(max_lenght=10, choices=TIPO_CHOICES)
+    tipo = models.CharField(max_length=10, choices=TIPO_CHOICES)
     criado_em = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.titulo} ({self.tipo})"
+        return f"Anúncio de {self.titulo} ({self.tipo})"
 
 
+class Item(models.Model):
+    TIPO_CHOICES = [
+        ('produtos', 'Produto'),
+        ('servico', 'Serviço')
+    ]
+
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField()
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    tipo = models.CharField(max_length=10, choices=TIPO_CHOICES)
+    criado_em = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.titulo}"
